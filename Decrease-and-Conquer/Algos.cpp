de741be81a67d03b6e51c64cd33804b4fcd76ac1
Andriy Kuz <andriy.kuzz@gmail.com>
2015-05-06 11:58:50 +0300

    Divide and conquer
    - Implement Merge and Quick sort (LomutoPartition and HoarePartition)
    - Implement QuickSelect

--- Decrease-and-Conquer/Algos.cpp
+++ Decrease-and-Conquer/Algos.cpp
@@ -6,6 +6,8 @@ using namespace std;
 int BinarySearch_NonRec(const vector<int>& Ar, int k);
 int TernarySearch_NonRec(const vector<int>& Ar, int k);
 int MultyplicationLaRus(int n, int m);
+int LomutoPartition(vector<int>& Ar, int l, int r);
+int QuickSelect(vector<int>& Ar, int l, int r, int k);
 
 int main()
 {
@@ -13,7 +15,9 @@ int main()
 	//test
 	//for (int i = 0; i <= Ar.size(); i++)
 	//	cout << TernarySearch_NonRec(Ar, i) << endl;
-	cout << MultyplicationLaRus(13, 94);
+	//cout << MultyplicationLaRus(13, 94);
+	vector<int> Ar = { 5,7,3,9,2 };
+	cout << QuickSelect(Ar, 0, Ar.size()-1, 1);
 	return 0;
 }
 
@@ -80,6 +84,31 @@ int MultyplicationLaRus(int n, int m)
 
 		return MultyplicationLaRus(n, m) + k;
 	}
+}
+
+int LomutoPartition(vector<int>& Ar, int l, int r)
+{
+	int p = Ar[l], s = l;
+	for (int i = l + 1; i <= r; i++)
+	{
+		if (Ar[i] < p)
+		{
+			std::swap(Ar[++s], Ar[i]);
+		}
+	}
+	std::swap(Ar[l], Ar[s]);
+	return s;
+}
+
+int QuickSelect(vector<int>& Ar, int l, int r, int k)
+{
+	int s = LomutoPartition(Ar, l, r);
+	if (s == k)
+		return Ar[s];
+	else if (k < s)
+		return QuickSelect(Ar, l, s - 1, k);
+	else
+		return QuickSelect(Ar, s+1, r, k);
 }
 
 
---
+++ Divide-and-Conquer/Sorting.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int FindMax(vector<int>& Ar, int l, int r);
+
+void MergeSort(vector<int>& Ar);
+void QuickSort(vector<int>& Ar, int l, int r);
+int LomutoPartition(vector<int>& Ar, int l, int r);
+int HoarePartition(vector<int>& Ar, int l, int r);
+
+int main()
+{
+	vector<int> Ar = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+	QuickSort(Ar, 0, Ar.size()-1);
+	for (auto v : Ar)
+		cout << v << ",";
+	cout << endl;
+	//cout << FindMax(Ar, 0, Ar.size()-1);
+	return 1;
+}
+
+void MergeSort(vector<int>& Ar)
+{
+	if (Ar.size() == 1)
+		return;
+	else
+	{
+		int m = Ar.size() / 2;
+		vector<int> B(Ar.begin(), Ar.begin() + m), C(Ar.begin() + m, Ar.end());
+		MergeSort(B);
+		MergeSort(C);
+		std::merge(B.begin(), B.end(), C.begin(), C.end(), Ar.begin());
+	}
+}
+
+int FindMax(vector<int>& Ar, int l, int r)
+{
+	if (l == r)
+		return l;
+	else
+	{
+		int m = (l + r) / 2;
+		int i1 = FindMax(Ar, l, m);
+		int i2 = FindMax(Ar, m + 1, r);
+		return Ar[i1] > Ar[i2] ? i1 : i2;
+	}
+}
+
+void QuickSort(vector<int>& Ar, int l, int r)
+{
+	if (l < r)
+	{
+		int s = HoarePartition(Ar, l, r);/*LomutoPartition(Ar, l, r)*/;
+		QuickSort(Ar, l, s - 1);
+		QuickSort(Ar, s + 1, r);
+	}
+}
+
+int LomutoPartition(vector<int>& Ar, int l, int r)
+{
+	int p = Ar[l], s = l;
+	for (int i = l + 1; i <= r; i++)
+	{
+		if (Ar[i] < p)
+		{
+			std::swap(Ar[++s], Ar[i]);
+		}
+	}
+	std::swap(Ar[l], Ar[s]);
+	return s;
+}
+
+int HoarePartition(vector<int>& Ar, int l, int r)
+{
+	int p = Ar[l], s = l, i = l + 1, j = r;
+
+	while (i < j)
+	{
+		while (i < r &&  Ar[i] < p)
+			++i;
+		
+		while (j > l && Ar[j] > p)
+			--j;
+
+		std::swap(Ar[i], Ar[j]);
+	}
+
+	std::swap(Ar[i], Ar[j]); // undo swap
+	std::swap(Ar[j], Ar[l]);
+	return j;
+}
